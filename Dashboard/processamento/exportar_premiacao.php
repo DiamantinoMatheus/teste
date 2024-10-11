ec10622e05e38ea052ea23904d410d7bbaf578c0
DiamantinoMatheus <matheus.ferreira@hrgama.com>
2024-10-11 13:13:20 -0300

    extra

--- Dashboard/processamento/exportar_premiacao.php
+++ Dashboard/processamento/exportar_premiacao.php
@@ -28,14 +28,14 @@ try {
     $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Consulta para selecionar os dados da tabela 'giros'
-    $sql = "SELECT nome, email, codigo, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') as created_at FROM giros"; // Ajuste a tabela conforme necessário
+    // Consulta para selecionar os dados da tabela 'premiacao'
+    $sql = "SELECT nome, email, codigo, whatsapp, DATE_FORMAT(created_at, '%Y-%m-%d %H:%i:%s') as created_at FROM premiacao"; // Ajuste a tabela conforme necessário
     $stmt = $conn->prepare($sql);
     $stmt->execute();
 
     // Define o cabeçalho do arquivo CSV
     header('Content-Type: text/csv; charset=utf-8');
-    header('Content-Disposition: attachment; filename="usuarios_giros_gratis.csv"');
+    header('Content-Disposition: attachment; filename="usuarios_premiacao.csv"');
 
     // Abre a saída para escrita
     $output = fopen('php://output', 'w');
@@ -44,7 +44,7 @@ try {
     fprintf($output, chr(0xEF) . chr(0xBB) . chr(0xBF));
 
     // Escreve o cabeçalho do CSV
-    fputcsv($output, ['Nome', 'Email', 'Codigo', 'Data/Hora'], ';'); // Usando ponto e vírgula como delimitador
+    fputcsv($output, ['Nome', 'Email', 'Codigo', 'Whatsapp' ,'Data/Hora'], ';'); // Usando ponto e vírgula como delimitador
 
     // Escreve os dados no CSV
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
